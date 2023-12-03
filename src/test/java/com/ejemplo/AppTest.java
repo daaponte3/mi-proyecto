a86dc9e52bff52b0b3cc829c942fa1e94483694f
daaponte3 <150068771+daaponte3@users.noreply.github.com>
2023-12-03 15:47:42 -0500

    Tests

--- src/test/java/com/ejemplo/AppTest.java
+++ src/test/java/com/ejemplo/AppTest.java
@@ -1,6 +1,5 @@
 package com.ejemplo;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
@@ -8,23 +7,24 @@ import java.io.PrintStream;
 
 import org.junit.Test;
 
+
 public class AppTest {
 	@Test
 	public void testApp() {
 		assertTrue(true);
 	}
 
-	 @Test
-	    public void testMainMethod() {
-	        // Redirigir la salida estándar a un ByteArrayOutputStream para capturarla
-	        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	        System.setOut(new PrintStream(outContent));
+	@Test
+	public void testMainMethod() {
+		// Redirigir la salida estándar a un ByteArrayOutputStream para capturarla
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
 
-	        // Llamar al método main de la clase App
-	        App.main(new String[]{});
+		// Llamar al método main de la clase App
+		App.main(new String[] {});
 
-	        // Restaurar la salida estándar
-	        System.setOut(System.out);
+		// Restaurar la salida estándar
+		System.setOut(System.out);
 
-	    }
+	}
 }
