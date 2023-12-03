cd488ab32848007d6d9760ce7e93097276a0fae1
daaponte3 <150068771+daaponte3@users.noreply.github.com>
2023-12-03 15:38:51 -0500

    corrección Test

--- src/test/java/com/ejemplo/AppTest.java
+++ src/test/java/com/ejemplo/AppTest.java
@@ -1,11 +1,30 @@
 package com.ejemplo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test
-    public void testApp() {
-        assertTrue(true);
-    }
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+
+	 @Test
+	    public void testMainMethod() {
+	        // Redirigir la salida estándar a un ByteArrayOutputStream para capturarla
+	        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	        System.setOut(new PrintStream(outContent));
+
+	        // Llamar al método main de la clase App
+	        App.main(new String[]{});
+
+	        // Restaurar la salida estándar
+	        System.setOut(System.out);
+
+	    }
 }
